ba2719de5aab79475e04b2fd1fa55385e509ee12
Kasp5953 <kasp5953@edu.easj.dk>
2017-09-01 08:39:25 +0200

    sorta doing it

---
+++ Yatzy/Yatzy/Evaluators/TwoPairEvaluator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Yatzy.Evaluators {
+    class TwoPairEvaluator : IDiceEvaluator {
+        public int Evaluate(Dictionary<int, int> diceCountByValue) {
+            int score = 0;
+            List<int> keys = new List<int>();
+
+            foreach(var dieCount in diceCountByValue) {
+                //first condition: more than 4 dice
+                //second condition: use highest pairs
+                //third condition: pairs must be different
+                if((dieCount.Value >= 4) && ()) {
+                    
+                }
+            }
+
+            return score;
+        }
+
+    }//end class
+}//end namespace
